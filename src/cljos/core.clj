ca560ea80a4adefacf06e498bef76489bdb4b3be
Divyansh Prakash <divyansh.prakash@yahoo.com>
2014-10-19 20:32:15 +0530

    Pretty close to the goal.

--- src/cljos/core.clj
+++ src/cljos/core.clj
@@ -1,18 +1,42 @@
 (ns cljos.core)
 
-(defmacro defclass [class-name & bindings]
-  (let [obj-map      (into {} (partition 2 bindings))
-        let-bindings (->> (get-in obj-map [:let] [])
-                          (partition 2)
-                          (mapcat #([(first %) `(atom ~(second %))])))
-        constructor  (get-in obj-map [:init] '([] nil))
-        methods      (into {} (filter #(not= (first %) :let :init) 
-                                      obj-map))]
-    `(defn ~class-name ~(first constructor)
-       (let [~@let-bindings]
-         ~@(rest ~constructor)
-         (fn [method# & args#]
-           (case method#
-             ~@(mapcat (fn [m-name# expr#]
-                         [~m-name# (do ~@expr#)])
-                       methods)))))))
+(defn class* [vars methods]
+  {:vars    vars
+   :methods (into {:init (fn [& args])}
+                  methods)})
+
+(defmacro defclass [name vars methods]
+  `(defn ~name []
+     (~class* ~vars ~methods)))
+
+(defn =<
+  "Get property of obj" 
+  [obj property]
+  (-> obj :vars property))
+
+(defn => 
+  "Call method on obj"
+  [obj method & args]
+  (apply (partial (-> obj :methods method) obj) args))
+
+;Usage:
+;
+;(defclass Stack 
+;  {:s (atom [])}
+;  {:init (fn [this & [xs]] 
+;           (swap! (=< this :s) concat xs))
+;   :push (fn [this x]
+;           (swap! (=< this :s) (comp #(conj % x) vec)))
+;   :pop  (fn [this]
+;           (let [x (last @(=< this :s))]
+;             (swap! (=< this :s) pop)
+;             x))
+;   :vec  (fn [this]
+;           (vec @(=< this :s)))})
+;
+;(let [s (Stack)]
+;  (=> s :init [1 2])
+;  (=> s :push 3) 
+;  (=> s :push 4)
+;  (=> s :pop)
+;  (=> s :vec)) ;=> [1 2 3]
