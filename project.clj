0219c8fd47fcf4c9ba2325c8c5f1a4ed0188b7ad
Divyansh Prakash <divyansh.prakash@yahoo.com>
2014-10-19 22:33:13 +0530

    Modified project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject cljos "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :description "CljOS - A toy OOP system on top of Clojure"
+  :url "http://github.com/divs1210/cljos"
+  :dependencies [[org.clojure/clojure "1.6.0"]])
