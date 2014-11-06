b370b941ddad6462a8ac6a645a42ba042d29fbdb
Divyansh Prakash <divyansh.prakash@yahoo.com>
2014-11-07 01:54:33 +0530

    Updated documentation. Changed class+ to make-class, and added doto+.

--- src/cljos/core.clj
+++ src/cljos/core.clj
@@ -1,26 +1,46 @@
 (ns cljos.core)
 
-(defn class+ [type extends vars methods]
+;-----The 'class' data structre-------------------
+(defn make-class
+  [type extends vars methods]
   {:vars  (or (into (extends :vars) vars)    {})
    :fns   (or (into (extends :fns ) methods) {})
    :type  type
    :super extends})
 
-(defmacro defclass [name extends vars methods]
+(defmacro defclass
+  "Every CljOS class should extend another.
+   Extend <Obj> if none other is applicable."
+  [name extends vars methods]
   `(def ~name
-     (~class+ (quote ~name)
-              ~extends
-              ~vars ~methods)))
+     (~make-class (quote ~name)
+                  ~extends
+                  ~vars ~methods)))
 
-(defn new+ [class-name & args]
+;-----Bootstrapping the CljOS base class----------
+(def <Obj*>
+  {:vars  {}
+   :fns   {:init (fn [_])}
+   :type  '<Obj*>
+   :super nil})
+
+(defclass <Obj> <Obj*> {} {})
+
+;-----Object instantiation------------------------
+(defn new+
+  "Instantiates a new CljOS object, which is a closure
+   with access to a set of methods, and *mutable* state." 
+  [class-name & args]
   (let [state (atom (class-name :vars))
         fns   (class-name :fns)
         this  (fn this* [method & argv]
                 (case method
                   ;special keywords
+                  :type  (class-name :type)
                   :state @state
                   :super (let [[method & args] argv]
-                           (apply (partial (-> class-name :super :fns method) this*)
+                           (apply (partial (-> class-name :super :fns method) 
+                                           this*)
                                   args))
                   :set   (let [[var val] argv]
                            (swap! state #(assoc % var val)))
@@ -34,14 +54,16 @@
                   ;a property.
                   (if (contains? fns method)
                     (apply (partial (fns method) this*) argv)
-                    (@state method))))]
+                    (get @state method))))]
+    ;call constructor, and return the object
     (apply (partial (fns :init) this) args)
     this))
 
-(def <Obj*>
-  {:vars  {}
-   :fns   {:init (fn [_])}
-   :type  '<Obj*>
-   :super nil})
-
-(defclass <Obj> <Obj*> {} {})
+;-----Utility macro(s)-----------------------------
+(defmacro doto+
+  "Like \"doto\", but for CljOS objects."
+  [obj & forms]
+  (cons 'do (for [form forms]
+              (if (seq? form)
+                (cons obj form)
+                (list obj form)))))
